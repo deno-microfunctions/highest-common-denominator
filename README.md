0c701ac8b3f86fc9fca43629795808de470b0b63
Michael <michael.spengler@sap.com>
2022-01-24 12:13:40 +0100

    improve tests as  documentation

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Highest Common Denominator
 
-## Usage Examples
+## Usage Example
 
 ```ts
 
+import { getHighestCommonDenominator } from "https://deno.land/x/getHighestCommonDenominator/mod.ts"
 
+console.log(getHighestCommonDenominator(21, 14))
 
 ```
 
-
-
 ---
-  
-For further examples you can check the [unit tests]().
+
+## Execute Usage Example  
+deno run https://deno.land/x/gethighestcommondenominator/src/gethighestcommondenominator.ts
+
+## Execute Unit Tests  
+
+deno test https://deno.land/x/gethighestcommondenominator/src/gethighestcommondenominator.spec.ts
 
 
--- src/getHighestCommonDenominator.spec.ts
+++ src/getHighestCommonDenominator.spec.ts
@@ -3,9 +3,14 @@ import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts"
 import { getHighestCommonDenominator } from "./getHighestCommonDenominator.ts"
 
 
-Deno.test("should generate a cool public and private key", async () => {
+Deno.test("should provide the highest common denominator", async () => {
 
-    const gcd = getHighestCommonDenominator(21, 14)
-    assertEquals(gcd, 7)
+    assertEquals(getHighestCommonDenominator(21, 14), 7)
+
+    assertEquals(getHighestCommonDenominator(18, 20000), 2)
+
+    assertEquals(getHighestCommonDenominator(23, 18), 1)
+
+    assertEquals(getHighestCommonDenominator(23, 18), 1)
 
 })
